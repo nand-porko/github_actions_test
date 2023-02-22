e2a8cbb1150d0d3c5e7fdcde7066e873d2fbf8ec
nand-porko <nandeeshvar1@gmail.com>
2023-02-21 23:28:47 -0500

    initial commit

---
+++ math.go
@@ -0,0 +1,18 @@
+// package math
+package main  
+ 
+import "fmt" 
+
+// Add is our function that sums two integers
+// func Add(x, y int) (res int) {
+// 	return x + y
+// }
+
+// // Subtract subtracts two integers
+// func Subtract(x, y int) (res int) {
+// 	return x - y
+// }
+ 
+func main() {  
+	fmt.Println("Hello World")  
+ }
